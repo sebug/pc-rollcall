a2eadf4e22227b547178f9e8d4b8f8fa22617bb7
sebug <sebastian.gfeller@gmail.com>
2020-04-22 17:12:33 +0200

    Reading text

--- reader.js
+++ reader.js
@@ -1,1 +1,15 @@
 console.log('Setting event handlers');
+
+const fileInput = document.querySelector('#pisa-xml');
+
+fileInput.addEventListener('change', () => {
+    if (fileInput.files.length) {
+	const firstFile = fileInput.files[0];
+	const reader = new FileReader();
+	reader.onload = (e) => {
+	    console.log(e.target.result);
+	};
+
+	reader.readAsText(firstFile);
+    }
+});
