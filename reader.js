89c09e9d42c78f8dbb61ef54f27ec1503bfeb46d
sebug <sebastian.gfeller@gmail.com>
2020-04-22 20:06:40 +0200

    Basic information

--- reader.js
+++ reader.js
@@ -17,5 +17,39 @@ fileInput.addEventListener('change', () => {
 const readPISAExport = (txt) => {
     const oParser = new DOMParser();
     const oDOM = oParser.parseFromString(txt, 'application/xml');
-    console.log(oDOM);
+    const daysNodes = Array.from(oDOM.querySelectorAll('PISA_TAGE_ADZS'));
+    const result = daysNodes.map(ParticipantDays.createFromNode);
+    console.log(result);
+    return result;
 };
+
+class ParticipantDays {
+    constructor() {
+    }
+
+    static createFromNode(node) {
+	console.log(node);
+	let result = new ParticipantDays();
+	const basicMappings = [
+	    [ 'employeeID', 'EMPLID' ],
+	    [ 'nationalID', 'NATIONAL_ID' ],
+	    [ 'eventID', 'ZS_ANLASSID' ],
+	    [ 'eventFromDate', 'ZS_ANL_VON_DT' ],
+	    [ 'participantType', 'ZS_TEILNART_CD' ],
+	    [ 'participantFromDate', 'ZS_TN_VON_DT' ],
+	    [ 'participantToDate', 'ZS_TN_BIS_DT' ],
+	    [ 'daysOfService', 'ZS_DIENSTTAGE' ],
+	    [ 'additionalTraining', 'ZS_ZUSAUSBG' ],
+	    [ 'mobile', 'ZS_MOBILE_HOME' ],
+	    [ 'email', 'ZS_EMAIL_ADDR' ],
+	    [ 'comments', 'ZS_DL_BEMERKUNG' ]
+	];
+	for (let m of basicMappings) {
+	    const v = node.querySelector(m[1]);
+	    if (v) {
+		result[m[0]] = v.innerHTML;
+	    }
+	}
+	return result;
+    }
+}
