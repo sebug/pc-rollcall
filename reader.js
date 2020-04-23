5cb1a63096d61ee14e1126fa36fbbcbac991329f
sebug <sebastian.gfeller@gmail.com>
2020-04-23 13:57:13 +0200

    Main participant properties

--- reader.js
+++ reader.js
@@ -11,20 +11,7 @@ fileInput.addEventListener('change', () => {
 	    const oDOM = oParser.parseFromString(e.target.result, 'application/xml');
 	    if (oDOM.querySelector('PISA_EO_TAGE_IN_OUT')) {
 		const participants = readDaysInOut(oDOM);
-		const tableNode = document.querySelector('.participants table');
-		const tableBody = tableNode.querySelector('tbody');
-		tableBody.innerHTML = ''; // clear out old content
-		const template = document.querySelector('#participant-row');
-		for (let participant of participants) {
-		    const participantRow = template.content.cloneNode('true');
-		    const td = participantRow.querySelectorAll('td');
-		    td[0].textContent = participant.email || '';
-		    td[1].textContent = participant.participantFromDate || '';
-		    td[2].textContent = participant.participantToDate || '';
-		    td[3].textContent = participant.daysOfService || '0';
-
-		    tableBody.appendChild(participantRow);
-		}
+		displayParticipants(participants);
 	    } else {
 		readCourseInfo(oDOM);
 	    }
@@ -33,6 +20,23 @@ fileInput.addEventListener('change', () => {
 	reader.readAsText(firstFile);
     }
 });
+
+const displayParticipants = (participants) => {
+    const tableNode = document.querySelector('.participants table');
+    const tableBody = tableNode.querySelector('tbody');
+    tableBody.innerHTML = ''; // clear out old content
+    const template = document.querySelector('#participant-row');
+    for (let participant of participants) {
+	const participantRow = template.content.cloneNode('true');
+	const td = participantRow.querySelectorAll('td');
+	td[0].textContent = participant.email || '';
+	td[1].textContent = participant.participantFromDate || '';
+	td[2].textContent = participant.participantToDate || '';
+	td[3].textContent = participant.daysOfService || '0';
+
+	tableBody.appendChild(participantRow);
+    }
+};
 
 const readDaysInOut = (oDOM) => {
     const daysNodes = Array.from(oDOM.querySelectorAll('PISA_TAGE_ADZS'));
@@ -129,6 +133,86 @@ class EventDescription {
 
 	const eventNodes = Array.from(node.querySelectorAll('ZS_ANL_DT_EFF'));
 	result.dates = eventNodes.map(EventDate.createFromNode);
+	const participantNodes = Array.from(node.querySelectorAll('ZS_ANLASS_TEILN'));
+	result.participants = participantNodes.map(Participant.createFromNode);
+	
+	return result;
+    }
+}
+
+class Participant {
+    constructor() {
+    }
+
+    static createFromNode(node) {
+	let result = new Participant();
+	const basicMappings = [
+	    [ 'employeeID', 'EMPLID' ],
+	    [ 'nationalID', 'NATIONAL_ID' ],
+	    [ 'oldNationalID', 'ZS_ALTE_AHV_NR' ],
+	    [ 'lastName', 'LAST_NAME' ],
+	    [ 'firstName', 'FIRST_NAME' ],
+	    [ 'fullName', 'ZS_NAME' ],
+	    [ 'birthDate', 'BIRTHDATE'],
+	    [ 'language', 'LANG_CD' ],
+	    [ 'address1', 'ADDRESS1'],
+	    [ 'address2', 'ADDRESS2' ],
+	    [ 'address3', 'ADDRESS3' ],
+	    [ 'zipCode', 'POSTAL' ],
+	    [ 'city', 'CITY' ],
+	    [ 'state', 'STATE' ],
+	    [ 'originZipCode', 'ZS_POSTAL_H' ],
+	    [ 'originCity', 'ZS_CITY_H' ],
+	    [ 'originState', 'ZS_STATE_H' ],
+	    [ 'occupation', 'ZS_BERUF_CD' ],
+	    [ 'occupationDescription', 'ZS_BERUF_DESCR'],
+	    [ 'function', 'ZS_FUNK_CD' ],
+	    [ 'functionDescription', 'ZS_FUNKT_DESCR' ],
+	    [ 'rank', 'ZS_GRAD_CD' ],
+	    [ 'rankDescription', 'ZS_GRAD_DESCR' ],
+	    [ 'departmentState', 'ZS_DEPTID_KT' ],
+	    [ 'departmentStateDescription', 'ZS_DEPTID_KT_DESCR' ],
+	    [ 'departmentOrganization', 'ZS_DEPTID_ORG' ],
+	    [ 'departmentOrganizationDescription', 'ZS_DEPTID_ORG_DESCR' ],
+	    [ 'department', 'ZS_DEPTID' ],
+	    [ 'departmentDescription', 'ZS_DEPTID_DESCR' ],
+	    [ 'eventID', 'ZS_ANLASSID' ],
+	    [ 'status', 'ZS_ANLTNSTATUS' ],
+	    [ 'statusDescription', 'ZS_ANLTNSTAT_DESCR' ],
+	    [ 'participantKind', 'ZS_TEILNART_CD' ],
+	    [ 'participantKindDescription', 'ZS_TLNART_DESCR' ],
+	    [ 'fromDate', 'ZS_TN_VON_DT' ],
+	    [ 'toDate', 'ZS_TN_BIS_DT' ],
+	    [ 'startTime', 'ZS_TN_EINR_TM' ],
+	    [ 'endTime', 'ZS_TN_ENTL_TM' ],
+	    [ 'place', 'ZS_TN_EINR_ORT_CD' ],
+	    [ 'placeDescription', 'ZS_TN_EORT_DESCR' ],
+	    [ 'placeAddress1', 'ZS_TN_EADDRESS1' ],
+	    [ 'placeAddress2', 'ZS_TN_EADDRESS2' ],
+	    [ 'placeZipCode', 'ZS_TN_EINR_POSTAL' ],
+	    [ 'placeCity', 'ZS_TN_EINR_CITY' ],
+	    [ 'placeState', 'ZS_TN_EINR_STATE' ],
+	    [ 'placeReferenceNumber', 'ZS_TN_REFNR' ],
+	    [ 'apgGroup', 'ZS_FKT_GRP_EO' ],
+	    [ 'apgDescription', 'ZS_EO_DESCR' ],
+	    [ 'apg', 'ZS_EO_CD' ],
+	    [ 'mobilePhone', 'ZS_MOBILE_HOME' ],
+	    [ 'businessPhone', 'ZS_PHONE_BUSN' ],
+	    [ 'emergencyPhone', 'ZS_PHONE_EMERG' ],
+	    [ 'homePhone', 'ZS_PHONE_HOME' ],
+	    [ 'email', 'ZS_EMAIL_ADDR' ],
+	    [ 'participantGroup', 'ZS_TEILNEHM_GRUPPE' ]
+	];
+	for (let m of basicMappings) {
+	    const v = node.querySelector(m[1]);
+	    if (v) {
+		result[m[0]] = v.innerHTML || null;
+	    } else {
+		result[m[0]] = null;
+	    }
+	}
+	const effectiveDateNodes = Array.from(node.querySelectorAll('ZS_TEILN_DT_EFF'));
+	result.dates = effectiveDateNodes.map(EffectiveDate.createFromNode);
 	
 	return result;
     }
